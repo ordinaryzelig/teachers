b5cc59c34752c2fb35fdd99fa2c73d608e5e6555
Jared Ning <jared@redningja.com>
2016-12-28 15:24:59 -0800

    Use normal _test file names. Fix seed bug.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,9 @@ require_relative 'schools_import'
 
 stephanie = User.create!(
   :first_name => 'Stephanie',
-  :last_name => 'Adler',
-  :category => 'teacher',
+  :last_name  => 'Adler',
+  :email      => 'stephanie@adler.com',
+  :category   => 'teacher',
 )
 
 piedmont_primary = School.where(:name => 'Piedmont Primary Es').first!
